c51391e1a8c8763b271fb643014ef6b37d20e1f0
LCTheLegend <Mathiaslc@hotmail.dk>
2022-10-11 18:04:34 +0200

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,35 @@
 #include <stdio.h>
 #include <math.h>
 #include <assert.h>
+
+extern "C"
+{
 #include "taylor_sine.h"
+#include "stack.h"
+}
 // Add your test cases for Exercise 1b,c) here
 
 int main(void)
 {
-    printf("%lf", taylor_sine(0.000, 4));
-    printf("%lf", sin(0.00));
+    /*
+    double test_cases[7] = {0.0, 0.5, 1.6, 2.5, 3.5, 5.0, 15.0};
+
+    for(int i = 0 ; i < 7 ; i++)
+    {
+        printf("Test function: %lf\n", taylor_sine(test_cases[i], 5));
+        printf("Built in sin function: %lf\n\n", sin(test_cases[i]));
+        
+    }
+    */
+    stack first;
+    initialize(&first);
+    push(5, &first);
+    printf("%d\n", pop(&first));
+    //printf("%d\n", pop(&first));
+    push(5, &first);
+    push(10, &first);
+    //printf("%d", empty(&first));
+    printf("%d\n", pop(&first));
+    printf("%d", pop(&first));
     return 0;
 }
--- src/taylor_sine.c
+++ src/taylor_sine.c
@@ -1,7 +1,8 @@
 #include "taylor_sine.h"
 #include <math.h>
+#include <stdio.h>
 
-int factorial(int x)
+double factorial(int x) //Factorial function makes to make the calculations easier
 {
     if(x == 0)
         return 1;
@@ -12,17 +13,17 @@ int factorial(int x)
 double taylor_sine(double x, int n)
 {
     /* implement your function here */
-    int count = 3;
-    double power = 5;
+    int count = 2; //Counter variable for determining if next step is - or + and how many terms should be calculated
+    double power = 5.0;
     double result = 0.0;
 
-    result = x - (pow(x,3)/factorial(3));
-    for(count ; count <= n ; count++)
+    result = x - (pow(x,3)/factorial(3)); //Result for two first terms
+    for(count ; count <= n+5 ; count++) //Question for n terms
     {
         if(count % 2 == 1)
             result -= (pow(x,power)/factorial(power));
         if(count % 2 == 0)
-            result +=  (pow(x,power)/factorial(power));
+            result += (pow(x,power)/factorial(power));
         power += 2;  
     }
     return result;
